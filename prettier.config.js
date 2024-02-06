6da8a7e068688b8eade70d68fdebe8a7253e83b0
Marcin Gościcki <marcin@goscicki.eu>
2024-02-06 23:46:48 +0100

    Configure Prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 100,
+	useTabs: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+	tailwindConfig: './tailwind.config.ts',
+};

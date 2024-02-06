27a66f14c7f017814895ecc86e4de0c3476aa558
Marcin Gościcki <marcin@goscicki.eu>
2024-02-07 00:16:23 +0100

    Make logo bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
 					className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
 					src="/next.svg"
 					alt="Next.js Logo"
-					width={180}
-					height={37}
+					width={360}
+					height={74}
 					priority
 				/>
 			</div>

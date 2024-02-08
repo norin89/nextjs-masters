8d301ea3fc9f1768643b6323694788eeed5c3526
Marcin Gościcki <marcin@goscicki.eu>
2024-02-08 19:40:03 +0100

    Build storybook to `./public`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/storybook",
+        destination: "/storybook/index.html",
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;

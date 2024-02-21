266eed80c66ed4355d1ce92b9cc0be7af25a2549
Marcin Gościcki <marcin@goscicki.eu>
2024-02-21 16:39:08 +0100

    Load data on Product Page from API

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,27 +1,26 @@
 import { Title } from '@/ui/atoms';
+import type { ProductType } from '@/types';
 
-export default function ProductPage({ params }: { params: { productId: string } }) {
+export default async function ProductPage({ params }: { params: { productId: string } }) {
 	const { productId } = params;
+	const res = await fetch(`https://naszsklep-api.vercel.app/api/products/${productId}`);
+
+	const product = (await res.json()) as ProductType;
 
 	return (
 		<div className="align-center flex flex-wrap items-center justify-center">
 			<div className="md:w-1/2 xl:w-1/3">
-				<img
-					src="https://prd.place/400?id=45"
-					alt="Energy Saving Light Bulbs"
-					className="w-full max-w-screen-sm"
-				/>
+				<div className="bg-white p-10 text-gray-600">
+					<img src={product.image} alt={product.title} className="w-full max-w-screen-sm" />
+				</div>
 			</div>
-			<div className="md:w-1/2 xl:w-2/3">
+			<div className="md:w-1/2 md:p-8 xl:w-2/3">
 				<header className="mb-8">
-					<span className="text-xs uppercase opacity-50">Product id: {productId}</span>
-					<Title level={1}>Energy Saving Light Bulbs</Title>
+					<strong className="text-xs uppercase opacity-50">{product.category}</strong>
+					<Title level={1}>{product.title}</Title>
 				</header>
-				<p>
-					Illuminate your space efficiently with our Energy Saving Light Bulbs! These eco-friendly
-					bulbs offer bright, long-lasting light while conserving energy and reducing your
-					electricity bills. Upgrade to sustainable lighting today!
-				</p>
+				<p className="my-4 text-lg">{product.description}</p>
+				<p className="my-4">{product.longDescription}</p>
 			</div>
 		</div>
 	);

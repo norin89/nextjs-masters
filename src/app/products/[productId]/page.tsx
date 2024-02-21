5a6e42c7dc2acceba540f6819a1087ca4a5c031d
Marcin Gościcki <marcin@goscicki.eu>
2024-02-21 17:36:03 +0100

    Add price on Product Page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from 'next';
 
 import { Title } from '@/ui/atoms';
 import type { ProductType } from '@/types';
+import { formatPrice } from '@/utils/formatPrice';
 
 export async function generateMetadata({
 	params,
@@ -26,14 +27,17 @@ export default async function ProductPage({ params }: { params: { productId: str
 	return (
 		<div className="align-center flex flex-wrap items-center justify-center">
 			<div className="md:w-1/2 xl:w-1/3">
-				<div className="bg-white p-10 text-gray-600">
+				<div className="mb-8 bg-white p-10 text-gray-600 md:mb-0">
 					<img src={product.image} alt={product.title} className="w-full max-w-screen-sm" />
 				</div>
 			</div>
 			<div className="md:w-1/2 md:p-8 xl:w-2/3">
 				<header className="mb-8">
-					<strong className="text-xs uppercase opacity-50">{product.category}</strong>
+					<strong className="mb-2 block self-end text-6xl font-medium text-pink-500">
+						{formatPrice(product.price)}
+					</strong>
 					<Title level={1}>{product.title}</Title>
+					<strong className="text-xs uppercase opacity-50">{product.category}</strong>
 				</header>
 				<p className="my-4 text-lg">{product.description}</p>
 				<p className="my-4">{product.longDescription}</p>

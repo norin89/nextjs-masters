d42cfb5b2d9ed4d375612bda1ba3513baa4c102c
Marcin Gościcki <marcin@goscicki.eu>
2024-02-27 01:28:39 +0100

    Global styles adjustment

--- src/ui/organisms/NavBar/NavBar.tsx
+++ src/ui/organisms/NavBar/NavBar.tsx
@@ -14,21 +14,15 @@ export const NavBar = <T extends string>({
 	className,
 	...props
 }: NavBarProps<T> & { className?: string }) => (
-	<nav
-		className={cx(
-			'sticky left-0 top-0 z-10 w-full shadow-md shadow-black/5 backdrop-blur-md',
-			className,
-		)}
-		{...props}
-	>
+	<nav className={cx('sticky left-0 top-0 z-10 w-full backdrop-blur-md', className)} {...props}>
 		<Links
 			links={menu}
-			className="flex w-full flex-wrap items-center justify-center border-b border-gray-200/90 bg-gray-100/90 py-2 lg:py-4 dark:border-gray-950/90 dark:bg-neutral-800/90"
+			className="flex w-full flex-wrap items-center justify-center border-b border-gray-200/90 bg-white/90 py-2 lg:py-4 dark:border-gray-900/90 dark:bg-neutral-950/90"
 		/>
 		{submenu && submenu?.length > 0 && (
 			<Links
 				links={submenu}
-				className="flex w-full flex-wrap items-center justify-center border-b border-gray-200/90 bg-gray-50/90 py-2 lg:py-4 dark:border-gray-950/90 dark:bg-neutral-900/90"
+				className="flex w-full flex-wrap items-center justify-center border-b border-gray-200/90 bg-gray-50/90 py-2 lg:py-4 dark:border-gray-900/90 dark:bg-neutral-900/90"
 			/>
 		)}
 	</nav>
